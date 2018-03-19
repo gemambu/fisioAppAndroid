cd03ffb3fc80c434d0606a18e48856b41246e8d2
Rodrigo Limpias <rodrigolimpiascossio@hotmail.com>
2018-03-19 10:23:07 +0100

    CalendarActivity created; CalendarFragment created + calendarListener for date change

---
+++ app/src/main/java/com/projectx/fisioapp/app/activity/CalendarActivity.kt
@@ -0,0 +1,13 @@
+package com.projectx.fisioapp.app.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.projectx.fisioapp.R
+
+class CalendarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+    }
+}
---
+++ app/src/main/java/com/projectx/fisioapp/app/fragment/CalendarFragment.kt
@@ -0,0 +1,74 @@
+package com.projectx.fisioapp.app.fragment
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.Toast
+
+import com.projectx.fisioapp.R
+
+class CalendarFragment : Fragment() {
+
+    lateinit var root: View
+    lateinit var calendarView: CalendarView
+    private var onSelectedDateListener: OnSelectedDateListener? = null
+
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        inflater?.let{
+            root = it.inflate(R.layout.fragment_calendar, container, false)
+            calendarView = root.findViewById(R.id.fisio_calendar)
+        }
+
+        return root
+    }
+
+
+    fun setCalendarListener(calendar: CalendarView){
+        calendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            //Note that months are indexed from 0. So, 0 means january, 1 means February, 2 means march etc.
+            val sb = StringBuilder()
+            sb.append(dayOfMonth).append(month + 1).append(year)
+            val date = sb.toString()
+            onSelectedDateListener?.onSelectedDate(date)
+        }
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        commonAttach(context)
+    }
+
+    override fun onAttach(activity: Activity?) {
+        super.onAttach(activity)
+        commonAttach(activity)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        onSelectedDateListener = null
+    }
+
+    fun commonAttach(listener: Any?) {
+        if (listener is OnSelectedDateListener) {
+            onSelectedDateListener = listener
+        }
+    }
+
+    interface OnSelectedDateListener {
+        fun onSelectedDate(date: String)
+    }
+
+
+
+
+
+
+}
