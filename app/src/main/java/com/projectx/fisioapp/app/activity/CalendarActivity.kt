cf5b232d11e558b511fc447bfce6c555913a15cd
Rodrigo Limpias <rodrigolimpiascossio@hotmail.com>
2018-03-20 11:46:22 +0100

    CalendarActivity showing; wasnt running because fragments in XML were missing 'name' property

--- app/src/main/java/com/projectx/fisioapp/app/activity/CalendarActivity.kt
+++ app/src/main/java/com/projectx/fisioapp/app/activity/CalendarActivity.kt
@@ -3,16 +3,20 @@ package com.projectx.fisioapp.app.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.projectx.fisioapp.R
+import com.projectx.fisioapp.app.fragment.AppointmentsListFragment
 import com.projectx.fisioapp.app.fragment.CalendarFragment
 
 class CalendarActivity : AppCompatActivity() {
 
     lateinit var calendarFragment: CalendarFragment
+    lateinit var appointmentsListFragment: AppointmentsListFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_calendar)
         setContentView(R.layout.activity_calendar)
 
         calendarFragment = supportFragmentManager.findFragmentById(R.id.calendar_fragment) as CalendarFragment
+        appointmentsListFragment = supportFragmentManager.findFragmentById(R.id.appointments_fragment) as AppointmentsListFragment
     }
 }
