05039a8b75cca7a99240b9b2fbbfa89ff993e50b
Rodrigo Limpias <rodrigolimpiascossio@hotmail.com>
2018-03-22 11:52:49 +0100

    AppointmentDetailActivity 'half' implemented

---
+++ app/src/main/java/com/projectx/fisioapp/app/activity/AppointmentDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.projectx.fisioapp.app.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.projectx.fisioapp.R
+
+class AppointmentDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_appointment_detail)
+    }
+}
