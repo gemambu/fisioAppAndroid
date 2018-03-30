1feb1d0233debd16325b40f7bcdabac589334782
Rodrigo Limpias <rodrigolimpiascossio@hotmail.com>
2018-03-30 17:37:18 +0200

    Appointment model x 'domain' created; Appointment mapper from Repo model to Domain model implemented

--- domain/src/main/java/com/projectx/fisioapp/domain/model/Appointment.kt
+++ domain/src/main/java/com/projectx/fisioapp/domain/model/Appointment.kt
@@ -1,14 +1,43 @@
 package com.projectx.fisioapp.domain.model
 
+import java.io.Serializable
 import java.util.*
 
 open class Appointment (val id: String,
                         val servicePrice: String,
                         val customerName: String,
+                        val customerAddress: String,
                         val isConfirmed: Boolean,
                         val isCancelled: Boolean,
                         val date: Date,
                         val latitude: String,
                         val longitude: String,
-                        val extraInfo: String
-                        )
+                        val extraInfo: String): Serializable {
+
+    // Useless?
+    /*init {
+        Appointments(ArrayList())
+    }*/
+}
+
+
+class Appointments(val appointments: MutableList<Appointment>): Aggregate<Appointment> {
+    override fun count(): Int = appointments.size
+
+    override fun all(): List<Appointment> = appointments
+
+    override fun get(position: Int): Appointment = appointments[position]
+
+    override fun add(element: Appointment) {
+        appointments.add(element)
+    }
+
+    override fun delete(position: Int) {
+        appointments.removeAt(position)
+    }
+
+    override fun delete(element: Appointment) {
+        appointments.remove(element)
+    }
+}
+
