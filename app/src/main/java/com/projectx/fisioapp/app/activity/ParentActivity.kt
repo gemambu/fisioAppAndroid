00b5d32a0808d3f5df8db79893ad7ac0234e4dfa
Carlos <carlos.companyperez@gmail.com>
2018-04-18 00:19:55 +0200

    FragmentTabs without image, but title.

--- app/src/main/java/com/projectx/fisioapp/app/activity/ParentActivity.kt
+++ app/src/main/java/com/projectx/fisioapp/app/activity/ParentActivity.kt
@@ -93,15 +93,12 @@ open class ParentActivity : AppCompatActivity() {
             when (item.itemId) {
                 R.id.ic_menu_calendar -> {
                     Router().moveToCalendarActivity(main)
-                    item.setEnabled(true)
                 }
                 R.id.ic_menu_products -> {
                     Router().moveToProductsActivity(main)
-                    item.setEnabled(true)
                 }
                 R.id.ic_menu_services -> {
                     Router().moveToServicesActivity(main)
-                    item.setEnabled(true)
                 }
             }
 
